16dc54d627d0dfdfbd25e9e42d24d77a4f2815ac
Daniel Tinivella <dtinivella@gmail.com>
2025-03-16 13:19:37 -0300

    add migrations and schema for users and validations

--- config/dev.exs
+++ config/dev.exs
@@ -3,7 +3,7 @@ import Config
 # Configure your database
 config :twix, Twix.Repo,
   username: "postgres",
-  password: "postgres",
+  password: "password",
   hostname: "localhost",
   database: "twix_dev",
   stacktrace: true,
