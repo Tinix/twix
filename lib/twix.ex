66a84d03465f9fd13af8d29c8f840af55a473205
Daniel Tinivella <dtinivella@gmail.com>
2025-03-18 11:02:09 -0300

    feat(TWIX139): add like functionality for posts
    - Added `add_like.ex` to handle adding likes to posts.
    - Added `create.ex` to handle post creation.
    - Updated `lib/twix.ex` to include like-related logic.

--- lib/twix.ex
+++ lib/twix.ex
@@ -1,7 +1,11 @@
 defmodule Twix do
   alias Twix.Users
+  alias Twix.Posts
 
   defdelegate create_user(params), to: Users.Create, as: :call
   defdelegate get_user(id), to: Users.Get, as: :call
   defdelegate update_user(params), to: Users.Update, as: :call
+
+  defdelegate create_post(params), to: Posts.Create, as: :call
+  defdelegate add_like_to_post(id), to: Posts.AddLike, as: :call
 end
